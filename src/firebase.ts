77dd73603ca6c7e514f81e11cb72a2d97ed43263
Jake Billings <jake@jakebillings.com>
2020-03-14 22:05:18 -0600

    Add: initial unstyled hospital view

---
+++ src/firebase.ts
@@ -0,0 +1,9 @@
+import firebase from 'firebase'
+const config = {
+  apiKey: "AIzaSyBZq9e31zx1SujkjheC1ynjXVuP_QRh1NA",
+  authDomain: "howmanybedsdotcom-3de41.firebaseapp.com",
+  databaseURL: "https://howmanybedsdotcom-3de41.firebaseio.com",
+  projectId: "howmanybedsdotcom-3de41",
+};
+firebase.initializeApp(config);
+export default firebase;
