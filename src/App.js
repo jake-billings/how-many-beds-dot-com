35046f2f3883bb6beaf68cab0a24c1cca00349bc
Jake Billings <jake@jakebillings.com>
2020-03-14 23:24:27 -0600

    Add: react router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from 'react-router-dom'
 import HospitalsView from './HospitalsView'
+
 // import firebase from './firebase'
 
-function App() {
+function App () {
   return (
-    <HospitalsView/>
-  );
+    <>
+      <Router>
+        <Switch>
+          <Route path="/hospitals">
+            <HospitalsView/>
+          </Route>
+        </Switch>
+      </Router>
+    </>
+  )
 }
 
-export default App;
+export default App
