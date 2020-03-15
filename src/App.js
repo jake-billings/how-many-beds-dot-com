cc4e3d54d74b6b71a0916f9770673456bfb78464
Jake Billings <jake@jakebillings.com>
2020-03-14 23:53:56 -0600

    Add: react bootstrap and nav

--- src/App.js
+++ src/App.js
@@ -3,16 +3,20 @@ import './App.css'
 import {
   BrowserRouter as Router,
   Switch,
-  Route,
-  Link, Redirect,
+  Route, Redirect,
 } from 'react-router-dom'
 import HospitalsView from './HospitalsView'
+import Navbar from 'react-bootstrap/Navbar'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 // import firebase from './firebase'
 
 function App () {
   return (
     <>
+      <Navbar bg="dark" expand="lg" variant="dark">
+        <Navbar.Brand bg="dark">HowManyBeds.com</Navbar.Brand>
+      </Navbar>
       <Router>
         <Switch>
           <Route path="/hospitals">
