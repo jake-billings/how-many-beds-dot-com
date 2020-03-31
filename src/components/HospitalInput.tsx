5141a73c419a21071c9333cef4283bb4496e1385
Henry Kaufman <hkaufman19@gmail.com>
2020-03-31 08:44:56 -0600

    Design: number inputs match textboxes, remove react-numeric-input

--- src/components/HospitalInput.tsx
+++ src/components/HospitalInput.tsx
@@ -4,7 +4,7 @@ import React, { useState, ChangeEvent } from 'react';
 import { Row, Col } from 'react-grid-system';
 
 import LocationInput from './LocationInput';
-import { Input, InputLabel, StyledNumericInput } from './ui/Input';
+import { Input, InputLabel } from './ui/Input';
 import Box from './ui/Box';
 
 import { Hospital } from '../types';
@@ -12,6 +12,7 @@ import { Text } from './ui/type';
 import { Flex } from './Flex';
 import Slider from 'rc-slider';
 import { colors } from './ui/variables';
+import Checkbox from './ui/Checkbox';
 
 type Props = {
   initialValue: Hospital | null;
@@ -52,8 +53,8 @@ export default function HospitalInput({ initialValue, onChange }: Props): JSX.El
     });
   };
 
-  const updateHospitalFieldWithEvent = (fieldName: string) => (e: ChangeEvent<HTMLInputElement>): void => {
-    const val = e?.target?.value;
+  const updateHospitalFieldWithEvent = (fieldName: string, num = false) => (e: ChangeEvent<HTMLInputElement>): void => {
+    const val = num && e?.target?.value ? +e?.target?.value.replace(/\+|-/, '') : e?.target?.value;
     if (!val) return;
     setHospital((h) => {
       const hospital = { ...h } as any;
@@ -115,20 +116,19 @@ export default function HospitalInput({ initialValue, onChange }: Props): JSX.El
         </Col>
         <Col sm={6}>
           <Box mb={1}>
-            <Flex center>
-              <Box mr={1}>
-                <input
-                  type="checkbox"
-                  checked={hospital.isCovidCenter}
-                  onChange={(): void => {
-                    updateHospitalField('isCovidCenter')(!hospital.isCovidCenter);
-                  }}
-                />
-              </Box>
-              <InputLabel>
-                Advertise this hospital as a <i>Covid Center</i>
-              </InputLabel>
-            </Flex>
+            <InputLabel>
+              <Flex center>
+                <Box mr={1}>
+                  <Checkbox
+                    checked={hospital.isCovidCenter}
+                    onChange={(): void => {
+                      updateHospitalField('isCovidCenter')(!hospital.isCovidCenter);
+                    }}
+                  />
+                </Box>
+                Advertise this hospital as a COVID Center
+              </Flex>
+            </InputLabel>
           </Box>
         </Col>
         <Col sm={6}>
@@ -137,29 +137,28 @@ export default function HospitalInput({ initialValue, onChange }: Props): JSX.El
           </Box>
         </Col>
         <Col sm={6}>
-          <Box mb={1}>
-            <Flex center>
-              <Box mr={1}>
-                <input
-                  type="checkbox"
-                  checked={hospital.sharingCovidPatientCount}
-                  onChange={(): void => {
-                    updateHospitalField('sharingCovidPatientCount')(!hospital.sharingCovidPatientCount);
-                  }}
-                />
-              </Box>
-              <InputLabel>Share Number of COVID Patients</InputLabel>
-            </Flex>
+          <Box mb={2}>
+            <InputLabel>
+              <Flex center>
+                <Box mr={1}>
+                  <Checkbox
+                    checked={hospital.sharingCovidPatientCount}
+                    onChange={(): void => {
+                      updateHospitalField('sharingCovidPatientCount')(!hospital.sharingCovidPatientCount);
+                    }}
+                  />
+                </Box>
+                Share Number of COVID Patients
+              </Flex>
+            </InputLabel>
           </Box>
           {hospital.sharingCovidPatientCount && (
-            <Box mb={11}>
+            <Box mb={3}>
               <InputLabel>COVID Patient Count</InputLabel>
-              <StyledNumericInput
-                type="number"
-                placeholder="500"
-                min={0}
+              <Input
+                type="text"
                 value={hospital.covidPatientCount}
-                onChange={updateHospitalField('covidPatientCount')}
+                onChange={updateHospitalFieldWithEvent('covidPatientCount')}
               />
             </Box>
           )}
@@ -170,14 +169,12 @@ export default function HospitalInput({ initialValue, onChange }: Props): JSX.El
           </Box>
         </Col>
         <Col sm={6}>
-          <Box mb={1}>
+          <Box mb={2}>
             <InputLabel>COVID Capable Beds</InputLabel>
-            <StyledNumericInput
-              type="number"
-              placeholder="500"
-              min={0}
+            <Input
+              type="text"
               value={hospital.covidCapableBedCount}
-              onChange={updateHospitalField('covidCapableBedCount')}
+              onChange={updateHospitalFieldWithEvent('covidCapableBedCount', true)}
             />
           </Box>
         </Col>
@@ -187,14 +184,12 @@ export default function HospitalInput({ initialValue, onChange }: Props): JSX.El
           </Box>
         </Col>
         <Col sm={6}>
-          <Box mb={1}>
+          <Box mb={2}>
             <InputLabel>ICU+COVID Capable Beds</InputLabel>
-            <StyledNumericInput
-              type="number"
-              placeholder="500"
-              min={0}
+            <Input
+              type="text"
               value={hospital.icuCovidCapableBedCount}
-              onChange={updateHospitalField('icuCovidCapableBedCount')}
+              onChange={updateHospitalFieldWithEvent('icuCovidCapableBedCount', true)}
             />
           </Box>
         </Col>
@@ -215,12 +210,10 @@ export default function HospitalInput({ initialValue, onChange }: Props): JSX.El
         <Col sm={6}>
           <Box mb={1}>
             <InputLabel>Ventilators</InputLabel>
-            <StyledNumericInput
-              type="number"
-              placeholder="500"
-              min={0}
+            <Input
+              type="text"
               value={hospital.ventilatorCount}
-              onChange={updateHospitalField('ventilatorCount')}
+              onChange={updateHospitalFieldWithEvent('ventilatorCount', true)}
             />
           </Box>
         </Col>
